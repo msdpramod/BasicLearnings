333a1208901ad8aa38a34be43c1e2584b4c6c8d9
msdpramod <kuppapramod@gmail.com>
2024-01-04 18:01:50 +0530

    updated

---
+++ src/main/java/org/javalearnings/basiclearnings/BasicLearningsApplication.java
@@ -0,0 +1,13 @@
+package org.javalearnings.basiclearnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicLearningsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicLearningsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javalearnings/basiclearnings/BasicLearningsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javalearnings.basiclearnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicLearningsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
